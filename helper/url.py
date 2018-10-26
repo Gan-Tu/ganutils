cd576164188c43e3bbac25bbf4c3a138a43ec17a
Gan Tu <tugan@berkeley.edu>
2018-10-26 16:56:41 -0700

    add basic url request script

---
+++ helper/url.py
@@ -0,0 +1,16 @@
+"""helper.url
+URL helpers
+"""
+
+import requests
+
+def fixURL(url):
+    assert type(url) == str, "URL has to be a string"
+    if url.startswith("www."):
+        url = "http://{}".format(url)
+    return url
+
+def request(url, params=None):
+    url = fixURL(url)
+    return requests.get(url).content
+
