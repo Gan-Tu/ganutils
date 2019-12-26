6a955ac55a0696714a9ddd88dac05b27c255064f
Gan Tu <tugan@berkeley.edu>
2019-12-25 16:29:04 -0800

    feature: add basic skeleton for releaseing package

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 # This directory is a Python package
 
 from __future__ import unicode_literals, print_function, division
-from utils import ai
-from utils import concurrency
-from utils import data
-from utils import file
-from utils import iterable
-from utils import url
+from ganutils import ai
+from ganutils import concurrency
+from ganutils import data
+from ganutils import file
+from ganutils import iterable
+from ganutils import url
