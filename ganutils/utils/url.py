9ff18ae340db597dd1b9eff3754b82f642140940
Gan Tu <tugan@berkeley.edu>
2019-12-26 13:13:41 -0800

    docs: make todo statements into sphnix directive

--- ganutils/utils/url.py
+++ ganutils/utils/url.py
@@ -5,15 +5,19 @@ URL helpers
 import requests
 
 def fixURL(url):
-    assert type(url) == str, "URL has to be a string"
-    # TODO(tugan): fixes more cases
-    if url.startswith("www."):
-        url = "http://{}".format(url)
-    return url
+  """
+  .. todo:: fixes more cases
+  """
+  assert type(url) == str, "URL has to be a string"
+  if url.startswith("www."):
+      url = "http://{}".format(url)
+  return url
 
 
 def request(url, params=None):
-    # TODO(tugan): use params as necessary
-    url = fixURL(url)
-    return requests.get(url).content
+  """
+  .. todo:: use params as necessary
+  """
+  url = fixURL(url)
+  return requests.get(url).content
 
