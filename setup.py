34ab9b057ce6a7ae37532fd037d25cc3fc90422a
Gan Tu <tugan@berkeley.edu>
2019-12-26 10:49:33 -0800

    chore: auto load readme as project description for pypi package

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
+
+def load_readme():
+  return open("./README.md", "r").read()
+
 setup(
   name = 'ganutils',
   packages = find_packages(),
-  version = '0.11',
+  version = '0.11.4',
   license='MIT',
   description = 'This is an installable python package for scripts I wrote for myself.',
+  long_description=load_readme(),
+  long_description_content_type='text/markdown',
   author = 'Gan Tu',
   author_email = 'tugan0329@gmail.com',
   url = 'https://github.com/Michael-Tu/ganutils',
