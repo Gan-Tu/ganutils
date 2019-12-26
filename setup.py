817a63860a65056f74d62a5c08c1b90a152438ab
Gan Tu <tugan@berkeley.edu>
2019-12-25 16:38:01 -0800

    update version for package release

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'ganutils',
   packages = ['ganutils'],
-  version = '0.1',
+  version = '0.2',
   license='MIT',
   description = 'This is an installable python package for scripts I wrote for myself.',
   author = 'Gan Tu',
   author_email = 'tugan0329@gmail.com',
   url = 'https://github.com/Michael-Tu/ganutils',
-  download_url = 'https://github.com/Michael-Tu/ganutils/archive/v_01.tar.gz',
+  download_url = 'https://github.com/Michael-Tu/ganutils/archive/v0.2.tar.gz',
   keywords = ['tugan', 'utils', 'tools'],
   install_requires=[
       'torch',
