4ad7ea833c15730c8dacf661a4d85f1ca3d446da
Gan Tu <tugan@berkeley.edu>
2019-12-25 16:29:04 -0800

    feature: add basic skeleton for releaseing package

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tu Gan's Utils
+
+This is an installable python package for scripts I wrote for myself.
---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+setup(
+  name = 'ganutils',
+  packages = ['ganutils'],
+  version = '0.1',
+  license='MIT',
+  description = 'This is an installable python package for scripts I wrote for myself.',
+  author = 'Gan Tu',
+  author_email = 'tugan0329@gmail.com',
+  url = 'https://github.com/Michael-Tu/ganutils',
+  download_url = 'https://github.com/Michael-Tu/ganutils/archive/v_01.tar.gz',
+  keywords = ['tugan', 'utils', 'tools'],
+  install_requires=[
+      'torch',
+      'numpy',
+      'sklearn',
+      'tqdm',
+      'requests',
+      'pickle',
+  ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
