94ce73ed78bf34e39a4de5f57d69beaaad17a1f7
Gan Tu <tugan@berkeley.edu>
2019-12-26 12:39:57 -0800

    chore: update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def load_readme():
 setup(
   name = 'ganutils',
   packages = find_packages(),
-  version = '0.12',
+  version = '0.12.2',
   license='MIT',
   description = 'This is an installable python package for scripts I wrote for myself.',
   long_description=load_readme(),
